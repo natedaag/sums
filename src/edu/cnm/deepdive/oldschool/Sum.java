28d89e0e38f271b8cfaa59f728d99e4a65c08e7e
Nathan <natedaage@yahoo.com>
2017-07-05 13:58:30 -0600

    dont remember

--- src/edu/cnm/deepdive/oldschool/Sum.java
+++ src/edu/cnm/deepdive/oldschool/Sum.java
@@ -5,11 +5,18 @@ public class Sum {
 public static void main(String[] args) {
 
 double sum = 0;
-  for (String arg : args) {
+  //for (String arg : args) {
+    //double value = Double.parseDouble(arg);
+    //sum += value; // sum = sum + value
+    
+  for (int i = 0; i < args.length; i++){
+    String arg = args [i];
     double value = Double.parseDouble(arg);
-    sum += value; // sum = sum + value
-  }
+    sum += value; // sum = sum + value;
+    System.out.println(value);
+  } // end for
+  
 	System.out.println(sum); 
- }
+ } // end main
 
-}
+}  // end Sum
